894a4b7440cee7eec6cf910688be86f4aca34b69
AdelOnTheTop <135359337+AdelOnTheTop@users.noreply.github.com>
2023-06-19 20:24:39 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/KingLegacy.lua"))()
-elseif  game.PlaceId == 2753915549 or  game.PlaceId == 4442272183or  game.PlaceId == 7449423635 then 
+elseif game.PlaceId == 2753915549 or  game.PlaceId == 4442272183 or  game.PlaceId == 7449423635 then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/AdelBloxFruit.lua"))()
 end
