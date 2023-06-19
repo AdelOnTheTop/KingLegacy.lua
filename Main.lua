ba4697543b438a4a28bfccd5ae918f1477f1d70c
memaybeohub <108448173+memaybeohub@users.noreply.github.com>
2023-06-19 19:39:15 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,6 @@
+v = game.PlaceId
 if game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/KingLegacy.lua"))()
+elseif v==2753915549 or v==4442272183or v==7449423635 then 
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/AdelBloxFruit.lua"))()
 end
