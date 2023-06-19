ddb9e4c4b99e46fd3195afd3dc0b1d220f67d3ff
AdelOnTheTop <135359337+AdelOnTheTop@users.noreply.github.com>
2023-06-19 18:42:45 +0700

    Update nhinconcac.lua

--- nhinconcac.lua
+++ nhinconcac.lua
@@ -1,13 +1,11 @@
-local SoBounty
-local TenPlayer
 local wehookbounty
 wehookbounty = hookfunction(require(game:GetService("ReplicatedStorage").Notification).new,function(...)
     local Args = {...}
     if Args[1] and string.find(Args[1], "killing") then 
         local BountyEarned = string.split(Args[1], "killing")
-        SendWH = true
-        SoBounty = string.gsub(BountyEarned[1],"%D","")
-        TenPlayer = BountyEarned[2]
+        getgenv().SendWH = true
+        getgenv().SoBounty = string.gsub(BountyEarned[1],"%D","")
+        getgenv().TenPlayer = BountyEarned[2]
         return wehookbounty(...)
     end
     return wehookbounty(...)
