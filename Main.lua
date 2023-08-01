d8dd5c9d64f06a172944e67232782c1361f04a1e
AdelOnTheTop <135359337+AdelOnTheTop@users.noreply.github.com>
2023-08-01 18:31:28 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-if not string.find(identifyexecutor(), 'Fluxus') then return game.Players.LocalPlayer:Kick("Only Support Fluxus") end
+
 if game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5931540094 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/KingLegacy.lua"))()
 elseif game.PlaceId==2753915549 or game.PlaceId==4442272183 or game.PlaceId==7449423635 then 
