7052d2dec3e1a7aab73e1086aeb2237feb4a71b4
AdelOnTheTop <135359337+AdelOnTheTop@users.noreply.github.com>
2023-10-10 19:29:22 +0700

    Update MainPre.lua

--- MainPre.lua
+++ MainPre.lua
@@ -1,4 +1,5 @@
-repeat wait() until game:IsLoaded()
-if game.PlaceId == 1537690962 then
-  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3a23f6a0871742be38cf5b5a6ce7bb48.lua"))()
-end
+pcall(function()
+  if game.PlaceId == 1537690962 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3a23f6a0871742be38cf5b5a6ce7bb48.lua"))()
+  end
+end)
