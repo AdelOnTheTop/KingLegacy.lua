6633ae1299541315fdab1da5cf7121c5d02ee4a5
AdelOnTheTop <135359337+AdelOnTheTop@users.noreply.github.com>
2024-01-06 12:36:44 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,8 +3,4 @@ if game.PlaceId == 4520749081 or game.PlaceId == 6381829480 or game.PlaceId == 5
   loadstring(game:HttpGet("https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/KingLegacy.lua"))()
 elseif game.PlaceId==2753915549 or game.PlaceId==4442272183 or game.PlaceId==7449423635 then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/AdelBloxFruits.lua"))()
-elseif game.PlaceId == 7465136166 or game.PlaceId == 3978370137 or game.PlaceId == 1730877806 then 
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/GrandPieceOnline.lua'))()
-elseif game.PlaceId == 11545598432 then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/AdelOnTheTop/Adel-Hub/main/AnimeFightingSimulatorX.lua'))()
 end
